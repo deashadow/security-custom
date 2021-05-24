20589fcf47530d88108374690126f44d2d5b3ac6
deashadow <67285924+deashadow@users.noreply.github.com>
2021-05-24 12:50:24 -0400

    Initial commit

---
+++ src/test/java/com/therealdanvega/SecurityCustomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.therealdanvega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityCustomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
